bac9f064a118773ba762826519fe90237297fec5
phu.hoang <phu.hoang@dsquare.com.vn>
2025-11-27 13:52:56 +0700

    update more example

---
+++ pointers/sample.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 10
+    var ptr *int
+    ptr = &a
+
+	fmt.Println("===== Example 1 =====")
+    fmt.Println("Value of a:", a)
+    fmt.Println("Address of a:", &a)
+    fmt.Println("Value of ptr:", ptr)
+    fmt.Println("Value pointed to by ptr:", *ptr)
+
+	fmt.Println("===== Example 2 =====")
+	fmt.Println("Value of a before:", a)
+    *ptr = 20
+    fmt.Println("Value of a after:", a)
+}
