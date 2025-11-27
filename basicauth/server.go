0a64569f2b49aceebd09a1076970459f3963bd02
phu.hoang <phu.hoang@dsquare.com.vn>
2025-11-27 13:54:45 +0700

    update more example

--- basicauth/server.go
+++ basicauth/server.go
@@ -1,36 +1,66 @@
 package main
 
 import (
-	"crypto/sha256"
+    "crypto/sha256"
     "crypto/subtle"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"time"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "time"
 )
 
 type application struct {
-	auth struct {
-		username string
-		password string
-	}
+    auth struct {
+        username string
+        password string
+    }
+}
+
+func (app *application) protectedHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "This is the protected handler")
+}
+
+func (app *application) unprotectedHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "This is the unprotected handler")
+}
+
+func (app *application) basicAuth(next http.HandlerFunc) http.HandlerFunc {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        username, password, ok := r.BasicAuth()
+        if ok {
+            usernameHash := sha256.Sum256([]byte(username))
+            passwordHash := sha256.Sum256([]byte(password))
+            expectedUsernameHash := sha256.Sum256([]byte(app.auth.username))
+            expectedPasswordHash := sha256.Sum256([]byte(app.auth.password))
+            usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+            passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+            if usernameMatch && passwordMatch {
+                next.ServeHTTP(w, r)
+                return
+            }
+        }
+
+        w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+    })
 }
 
 func main() {
-	app := new(application)
-	
-	app.auth.username = os.Getenv("USERNAME")
-	if password := os.Getenv("PASSWORD"); len(password) > 0 {
-		app.auth.password = password
-	} else {
-		app.auth.password = "$jSZ9y})haSLp>LB"
-	}
-	
-	fmt.Println(app.auth)
-	// initializing server
-	mux := http.NewServeMux()
-	mux.HandleFunc("/unprotected", app.unprotectedHandler)
+    app := new(application)
+    
+    app.auth.username = os.Getenv("USERNAME")
+    if password := os.Getenv("PASSWORD"); len(password) > 0 {
+        app.auth.password = password
+    } else {
+        app.auth.password = "$jSZ9y})haSLp>LB"
+    }
+    
+    fmt.Println(app.auth)
+    // initializing server
+    mux := http.NewServeMux()
+    mux.HandleFunc("/unprotected", app.unprotectedHandler)
     mux.HandleFunc("/protected", app.basicAuth(app.protectedHandler))
 
     srv := &http.Server{
@@ -44,34 +74,4 @@ func main() {
     log.Printf("starting server on %s", srv.Addr)
     err := srv.ListenAndServe()
     log.Fatal(err)
-}
-
-func (app *application) protectedHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "This is the protected handler")
-}
-
-func (app *application) unprotectedHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "This is the unprotected handler")
-}
-
-func (app *application) basicAuth(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		username, password, ok := r.BasicAuth()
-		if ok {
-			usernameHash := sha256.Sum256([]byte(username))
-			passwordHash := sha256.Sum256([]byte(password))
-			expectedUsernameHash := sha256.Sum256([]byte(app.auth.username))
-			expectedPasswordHash := sha256.Sum256([]byte(app.auth.password))
-			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
-			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
-
-			if usernameMatch && passwordMatch {
-				next.ServeHTTP(w, r)
-				return
-			}
-		}
-
-		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-	})
 }
--- online-salon-cicd/utils/common.go
+++ online-salon-cicd/utils/common.go
@@ -1,34 +1,34 @@
 package common
 
 import (
-	"bytes"
-	"fmt"
-	"os"
-	"os/exec"
+    "bytes"
+    "fmt"
+    "os"
+    "os/exec"
 )
 
 func ExecuteCommand(tool string, args []string, path string) (string, string, bool) {
-	cmd := exec.Command(tool, args...)
-	cmd.Dir = path
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	var errout string
-	var abort bool = false
+    cmd := exec.Command(tool, args...)
+    cmd.Dir = path
+    var stdout bytes.Buffer
+    var stderr bytes.Buffer
+    cmd.Stdout = &stdout
+    cmd.Stderr = &stderr
+    var errout string
+    var abort bool = false
 
-	if err := cmd.Run(); err != nil {
-		errout = fmt.Sprint(err) + ": " + stderr.String()
-		abort = true
-	}
+    if err := cmd.Run(); err != nil {
+        errout = fmt.Sprint(err) + ": " + stderr.String()
+        abort = true
+    }
 
-	return stdout.String(), errout, abort
+    return stdout.String(), errout, abort
 }
 
 func ConsoleLog(out string, errout string, abort bool) {
-	fmt.Println(out)
-	fmt.Println(errout)
-	if (abort) {
-		os.Exit(1)
-	}
+    fmt.Println(out)
+    fmt.Println(errout)
+    if (abort) {
+        os.Exit(1)
+    }
 }
