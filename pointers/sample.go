161bd3fb6d1c801ed81d5c887c0377a0adb150ac
phu.hoang <phu.hoang@dsquare.com.vn>
2025-12-09 18:20:52 +0700

    new pointer example

--- pointers/sample.go
+++ pointers/sample.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+func MultipleOfTen(factor *int) *int {
+	v := *factor * 10
+	return &v
+}
+
 func main() {
 	var a int = 10
     var ptr *int
@@ -19,4 +24,11 @@ func main() {
 	fmt.Println("Value of a before:", a)
     *ptr = 20
     fmt.Println("Value of a after:", a)
+	
+	fmt.Println("===== Example 3 =====")
+	var p int = 40
+	addr := MultipleOfTen(&p)
+	val := *MultipleOfTen(&p)
+	fmt.Println("Address:", addr)
+	fmt.Println("Return value:", val)
 }
